dee720433a6795d77408fb0119ff2ad85f0ad1d6
S906718 <S906718@S906718-W10>
2022-09-14 18:51:54 +0530

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using PTCData;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            TrainingProductViewModel vm = new TrainingProductViewModel();
+            vm.HandleRequest();
+            return View(vm);
+        }
+        [HttpPost]
+        public ActionResult Index(TrainingProductViewModel vm)
+        {
+            vm.IsValid = ModelState.IsValid;
+            vm.HandleRequest();
+            if (vm.IsValid)
+            {
+                ModelState.Clear();
+            }
+            
+            return View(vm);
+        }
+    }
+}
